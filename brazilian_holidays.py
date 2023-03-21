c15a33942bf8a8ca2d2fb63fb41bc4c14d74a570
Marcio Alves <marciosantosvitoralves@gmail.com>
2023-03-21 17:26:55 -0300

    adding python scrapping with ptax

---
+++ brazilian_holidays.py
@@ -0,0 +1,12 @@
+import holidays
+from typing import List, Dict, Iterable, Optional, Union
+
+class HolidaysBr(holidays.BR):
+
+    def __init__(self, 
+                years: Optional[Union[int, Iterable[int]]] = None, 
+                expand: bool = True, observed: bool = True, 
+                subdiv: Optional[str] = None, 
+                prov: Optional[str] = None, state: Optional[str] = None, 
+                language: Optional[str] = None) -> None:
+        super().__init__(years, expand, observed, subdiv, prov, state, language)
